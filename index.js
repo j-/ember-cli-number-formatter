9ba5b258983922e918277ef234d7041a5da3a9b4
j- <jh@live.com.au>
2015-04-14 15:35:16 +1000

    Added `treeFor` from `ic-ajax`

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+
+var unwatchedTree = function (dir) {
+  return {
+    read:    function() { return dir; },
+    cleanup: function() { }
+  };
+};
+
 module.exports = {
   name: 'number-formatter',
 
-  init: function () {
-    this.treePaths['vendor'] = 'node_modules';
+  treeFor: function (name) {
+    if (name === 'vendor') {
+      var treePath = path.join('node_modules', 'number-formatter', 'lib');
+      return unwatchedTree(treePath);
+    }
+    return this._super.treeFor.apply(this, arguments);
   },
 
-  included: function (app) {
-    this._super.included(app);
-    app.import('vendor/number-formatter/lib/format.js');
+  included: function (app, parentAddon) {
+    var target = parentAddon || app;
+    target.import('vendor/format.js');
   }
 };
